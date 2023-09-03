13226d18de8ec511763886aa0f3e88f95d8732e8
StandHereHermawan <nolifegansaya@gmail.com>
2023-09-03 17:34:38 +0700

    Request Method, modify controller class

--- src/main/java/programmerzamannow/webmvc/controller/HelloController.java
+++ src/main/java/programmerzamannow/webmvc/controller/HelloController.java
@@ -4,11 +4,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import programmerzamannow.webmvc.service.HelloService;
 
 import java.io.IOException;
-import java.util.Objects;
 
 @Controller
 public class HelloController {
@@ -16,7 +15,7 @@ public class HelloController {
     @Autowired
     private HelloService helloService;
 
-    @RequestMapping(path = "/hello")
+    @GetMapping(path = "/hello")
     public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String name = request.getParameter("name");
         String responseBody = helloService.hello(name);
