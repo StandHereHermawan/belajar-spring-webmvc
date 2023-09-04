eda15600767af7b7097dd5f4e501314046d9090d
StandHereHermawan <nolifegansaya@gmail.com>
2023-09-04 09:22:33 +0700

    Request Param, modify Controller class

--- src/main/java/programmerzamannow/webmvc/controller/HelloController.java
+++ src/main/java/programmerzamannow/webmvc/controller/HelloController.java
@@ -1,10 +1,10 @@
 package programmerzamannow.webmvc.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import programmerzamannow.webmvc.service.HelloService;
 
 import java.io.IOException;
@@ -16,8 +16,8 @@ public class HelloController {
     private HelloService helloService;
 
     @GetMapping(path = "/hello")
-    public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String name = request.getParameter("name");
+    public void helloWorld(@RequestParam(name = "name", required = false) String name,
+                           HttpServletResponse response) throws IOException {
         String responseBody = helloService.hello(name);
         response.getWriter().println(responseBody);
     }
