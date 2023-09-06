7a72242690db6f53f995aed311ecfa5c40788ec2
StandHereHermawan <nolifegansaya@gmail.com>
2023-09-06 11:18:14 +0700

    Json, add unit test

---
+++ src/test/java/programmerzamannow/webmvc/controller/PersonApiControllerTest.java
@@ -0,0 +1,57 @@
+package programmerzamannow.webmvc.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import programmerzamannow.webmvc.model.CreatePersonRequest;
+import programmerzamannow.webmvc.model.CreateSocialMediaRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PersonApiControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void createPerson() throws Exception {
+        CreatePersonRequest request = new CreatePersonRequest();
+        request.setFirstName("Hilmi");
+        request.setMiddleName("Akbar");
+        request.setLastName("Muharrom");
+        request.setEmail("imia@contoh.com");
+        request.setPhone("085169699696");
+        request.setHobbies(List.of("Baca Manga","Nonton Youtube","Berbagi Humor rasis"));
+        request.setSocialMedias(new ArrayList<>());
+        request.getSocialMedias().add(new CreateSocialMediaRequest("Facebook","facebook.com/Reimi"));
+        request.getSocialMedias().add(new CreateSocialMediaRequest("Instagram","instagram.com/k4kkoi"));
+
+        String jsonRequest = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                post("/api/person")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest)
+        ).andExpectAll(
+                status().isOk(),
+                content().json(jsonRequest)
+        );
+    }
+}
